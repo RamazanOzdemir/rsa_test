7dfe923457642763913497cf47eaf2752d84eb72
ramazanozdemir <ramazanozdemir2607@gmail.com>
2020-12-02 23:10:16 +0300

    first

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+import JSEncrypt from 'jsencrypt';
+import { useEffect } from 'react';
+
 
 function App() {
+  let token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTA5LCJpYXQiOjE2MDY4OTQ4MTMsImV4cCI6MTYwNjk4MTIxM30.5Lp7Uvc-1SSmqRzg6hQYejtT9s4oZ8RwA38MV6KM_hg';
+  useEffect(()=>{
+    const rsa_public_key = `-----BEGIN ${process.env.REACT_APP_RSA_KEY_FORMAT} KEY-----${process.env.REACT_APP_RSA_PUBLIC_KEY}-----END ${process.env.REACT_APP_RSA_KEY_FORMAT} KEY-----`;
+    function encryptMessage(message, publicKey) {
+      const jsEncrypt = new JSEncrypt();
+      jsEncrypt.setPublicKey(publicKey);
+      return jsEncrypt.encrypt(message);
+    }
+    let e_key = encryptMessage(token, rsa_public_key);
+
+    console.log("rsa_public_key  ->", rsa_public_key);
+    console.log("encrypted key ->", e_key);
+
+  },[]);
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          RSA TEST APP
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
